e4eb24d36840860e7b6eb5efadbdc57bfc9b89a7
Chandan <chandancvp08@gmail.com>
2021-12-15 14:45:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const Gun = require('gun')
+
+const app = express()
+const port = 8765
+app.use(Gun.serve)
+
+const server = app.listen(port, () => {
+    console.log("Listening at: http://localhost://" + port)
+})
+
+Gun({web: server})
+
+app.get("/", (req, res)=>{
+    res.send("hii this is the turse backend relay server, this node is a place where your data will be backed up in case everynode goes offline. Thank you for useing Turse!! Lets grow this network")
+    } )
+app.get("/gun",(req, res)=>{
+    res.send("hiii, you have reached the network relay server!!")
+})
