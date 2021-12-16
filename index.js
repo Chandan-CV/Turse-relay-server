003caa5b4341bb2a23388244802c0dce7b97afde
Chandan <chandancvp08@gmail.com>
2021-12-16 17:06:20 +0530

    initialized on heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Gun = require('gun')
 
 const app = express()
-const port = 8765
+const port = process.env.PORT || 8765
 app.use(Gun.serve)
 
 const server = app.listen(port, () => {
